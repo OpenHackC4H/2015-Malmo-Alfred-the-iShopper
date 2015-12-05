fa8617b527bd3774fa4f1b97466914b92b205d40
Sebastian Axelsen <s.axelsen89@gmail.com>
2015-12-05 14:58:35 +0100

    Adds new AudioManager class and mock audio resource

---
+++ alfred/Managers/AudioManager.h
@@ -0,0 +1,13 @@
+//
+//  AudioManager.h
+//  alfred
+//
+//  Created by Sebastian Axelsen on 05/12/15.
+//  Copyright © 2015 TeamYAY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioManager : NSObject
+
+@end
---
+++ alfred/Managers/AudioManager.m
@@ -0,0 +1,13 @@
+//
+//  AudioManager.m
+//  alfred
+//
+//  Created by Sebastian Axelsen on 05/12/15.
+//  Copyright © 2015 TeamYAY. All rights reserved.
+//
+
+#import "AudioManager.h"
+
+@implementation AudioManager
+
+@end
